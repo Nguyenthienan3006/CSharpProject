23677ff71e40bd1f298e63e91188eb2d32251d55
Nguyenthienan3006 <92444045+Nguyenthienan3006@users.noreply.github.com>
2023-09-21 16:38:25 +0700

    AN
    Upload new project

---
+++ MyCarStore/MyCarStore/MyCarStore/Controllers/CarController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCarStore.Models;
+using System.Web;
+
+namespace MyCarStore.Controllers
+{
+    public class CarController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Tao một list chứa danh sách các ô tô 
+            List<Car> cars = new List<Car>()
+            {
+                new Car { Id = 1, Name="Audi A8", Description="Expensive Car", Price = 100 },
+                new Car { Id = 2, Name="Kia Morning", Description="Cheap Car", Price = 20 },
+                new Car { Id = 3, Name="Lamboghini Aventador", Description="Super Car", Price = 500 },
+                new Car { Id = 4, Name="Roll Royces", Description="Super Car", Price = 400 },
+            };
+            return View(cars);
+        }
+
+
+        public string AboutUs(string name, int age)
+        {
+            return HttpUtility.HtmlEncode("Hello, I am " + name + ".  I am " + age + " years old.");
+        }
+
+    }
+}
---
+++ MyCarStore/MyCarStore/MyCarStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyCarStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
